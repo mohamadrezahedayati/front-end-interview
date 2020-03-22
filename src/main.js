671511553007e84422b815a115116c3360445dca
hedayati.m <hedayati.m.orchid@gmail.com>
2020-03-22 16:56:03 +0430

    fix app

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import App from './App.vue';
 import { routes } from './routers';
 import { store } from './store/store';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 Vue.use(VueRouter);
-
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 
 Vue.config.productionTip = false
 
