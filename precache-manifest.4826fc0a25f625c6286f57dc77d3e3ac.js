4956f99af06049c0ec7fdc6f8bd82118578563e1
hedayati.m <hedayati.m.orchid@gmail.com>
2020-04-02 16:17:24 +0430

    add index

---
+++ precache-manifest.4826fc0a25f625c6286f57dc77d3e3ac.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a6cf17334c956559362",
+    "url": "/css/app.1fcf8cfa.css"
+  },
+  {
+    "revision": "f4816372526313c2b5da",
+    "url": "/css/chunk-vendors.3b3b89f8.css"
+  },
+  {
+    "revision": "59824c6dfc2630111f0e622044c8218a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a6cf17334c956559362",
+    "url": "/js/app.cde61d1d.js"
+  },
+  {
+    "revision": "f4816372526313c2b5da",
+    "url": "/js/chunk-vendors.6839d30c.js"
+  },
+  {
+    "revision": "c459107be743c13ec9364ae9b8a7a41b",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "abf26c8f156dc97c79230abb9a5741ac",
+    "url": "/style/style.css"
+  }
+]);
